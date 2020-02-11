86f6fc2628d6ae076a34799b617c4309ac3e8ab9
bekimsylqazs <bekimsylqa@gmail.com>
2020-02-11 09:59:31 +0100

    Perfundimi

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@
 
 Auth::routes();
 
+Route::get('/email',function (){
+    return new \App\Mail\NewUserWelcomeMail();
+});
+
 Route::post('follow/{user}','FollowsController@store');
 
 Route::get('/', 'PostsController@index');
